d4d30ab75c3e9fe3b9b0123d27123db6bff41f6a
Alper Silistre <alpersilistre@gmail.com>
2019-01-04 01:14:56 +0300

    Events example added.

--- app.js
+++ app.js
@@ -4,11 +4,24 @@ new Vue({
     name: 'Alper',
     job: 'Software Engineer',
     website: 'https://www.alpersilistre.com',
-    websiteTag: '<a href="https://www.alpersilistre.com">Alper Silistre Site</a>'
+    websiteTag: '<a href="https://www.alpersilistre.com">Alper Silistre Site</a>',
+    age: 26,
+    x: 0,
+    y: 0
   },
   methods: {
     greet: function (time) {
       return `Good ${time} ${this.name}`;
+    },
+    subtract: function () {
+      this.age--;
+    },
+    addTen: function () {
+      this.age += 10;
+    },
+    updateXY: function (event) {
+      this.x = event.offsetX;
+      this.y = event.offsetY;
     }
   }
 });
